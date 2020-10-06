54f06b3fc2c37b1cce0601e027ffd07ac397e669
Ujjawal Pabreja <ujjawalpabreja@Ujjawals-MacBook-Pro.local>
2020-10-06 09:24:25 +0530

    #666_A_to_E_added

---
+++ Div2/#666/c.cpp
@@ -0,0 +1,104 @@
+/*
+    Ujjawal Pabreja [cuber_coder]
+*/
+#include <bits/stdc++.h> 
+#include <functional> 
+using namespace std;
+#define speed_cubing    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define ll              long long
+#define ld              long double
+#define rep(i,a)        for(ll i = 0 ; i < a ; i++)
+#define repe(i,a,b)     for(ll i = a ; i < b ; i++)
+#define bac(i,a)        for(ll i = a ; i >= 0 ; i--)
+#define bace(i,a,b)     for(ll i = a ; i >= b ; i--)
+#define pb              push_back
+#define in              insert
+#define ff              first
+#define ss              second
+#define setbit(x)       __builtin_popcountll(x)
+#define init(c,a)       memset(c,a,sizeof(c))
+#define all(c)          c.begin(),c.end()
+#define sz(c)           (ll)c.size()
+#define lb              lower_bound
+#define ub              upper_bound
+#define maxe            *max_element
+#define mine            *min_element
+#define rev             reverse
+#define endl            "\n"
+#define debug(x)        cout << #x << " : " << x << endl;
+const ll inf = 1e18;  
+const ll mod = 1e9 + 7;
+const ll MOD = 998244353;
+const ll MAX = 2e5 + 1;
+
+inline ll add(ll a,ll b) {return ((a % mod) + (b % mod)) % mod;}
+inline ll sub(ll a,ll b) {return ((a % mod) - (b % mod) + mod) % mod;}
+inline ll mul(ll a,ll b) {return ((a % mod) * (b % mod)) % mod;}
+ll pwr(ll x,ll n){
+    if(n == 0){
+        return 1;
+    }
+    if(n&1){
+        return mul(x, pwr(mul(x, x), (n - 1) / 2));
+    } else {
+        return pwr(mul(x, x), n / 2);
+    }
+}
+ll modInv(ll n){ 
+    return pwr(n, mod - 2); 
+}
+ll gcd(ll a,ll b) {
+    if(b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+// ====== Useful functions end ======
+ 
+// ====== Global Variables begin ======
+
+// ====== Global Variables end ======
+
+void speed_solve(){
+    ll n;
+    cin >> n;
+    ll a[n];
+    rep(i,n) {
+        cin >> a[i];
+    }
+    if(n == 1){
+        cout << 1 << " " << n << endl << -a[0] << endl;
+        cout << 1 << " " << n << endl << 0 << endl;
+        cout << 1 << " " << n << endl << 0 << endl;
+        return;
+    }
+    cout << 1 << " " << 1 << endl;
+    cout << -a[0] << endl;
+    a[0] = 0;
+    cout << 2 << " " << n << endl;
+    repe(i,1,n){
+        cout << (n - 1) * (a[i] % n) << " ";
+        a[i] += (n - 1) * (a[i] % n);
+    }
+    cout << endl;
+    cout << 1 << " " << n << endl;
+    rep(i,n){
+        cout << -a[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    speed_cubing;
+    ll test_cases = 1;
+    // cin >> test_cases;
+    rep(i,test_cases){
+        // cout << "Case #" << i + 1 << ": ";
+        speed_solve();
+    }
+    return 0;
+}
