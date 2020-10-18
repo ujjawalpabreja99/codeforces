48b750abedd15affe846d070c16d880b1281f7e1
Ujjawal Pabreja <ujjawalpabreja@Ujjawals-MacBook-Pro.local>
2020-10-19 02:10:29 +0530

    added A to D of codeforces round 676 (div2)

---
+++ Codeforces/Div2/#676/A_XORwice.cpp
@@ -0,0 +1,31 @@
+/**
+ * Written by : Ujjawal Pabreja [cuber_coder]
+ * Email : ujjawalpabreja99@gmail.com
+*/
+#include <bits/stdc++.h> 
+using namespace std;
+#define int long long
+#define ll long long
+#define ld long double
+#define endl "\n"
+const int mod = 1e9 + 7;
+const int MAX = 2e5 + 1;
+
+void Solve() {
+    ll a, b;
+    cin >> a >> b;
+    cout << (a ^ b) << endl;
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int testcases = 1;
+    cin >> testcases;
+
+    while (testcases--) {
+        Solve();
+    }
+}
